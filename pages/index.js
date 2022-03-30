709fc700806d39214c933e5e6b814e5c6e4516d5
achingachris <achinga.chris@gmail.com>
2022-03-30 03:18:32 +0300

    update

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,22 @@ import Head from 'next/head'
 import Image from 'next/image'
 
 export default function Home() {
+  const image_list = [
+    {
+      id: 989,
+      name: 'piano with brand text',
+      url: 'https://res.cloudinary.com/demo/image/upload/piano.jpg',
+    },
+    {
+      id: 990,
+      name: 'piano with blurred brand text',
+      url: 'https://res.cloudinary.com/demo/image/upload/e_pixelate_region:20,g_ocr_text/piano.jpg',
+    },
+  ]
   return (
     <>
       <Head>
         <title>Image Text Blur</title>
-        {/* <!-- Bootstrap CSS only --> */}
         <link
           href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css'
           rel='stylesheet'
@@ -18,34 +29,23 @@ export default function Home() {
         <h1 className='text-center m-4'>Text Image Blur</h1>
         <div className='container'>
           <div className='row'>
-            <div className='col'>
-              <div className='card'>
-                <Image
-                  src='https://res.cloudinary.com/demo/image/upload/e_pixelate_region:20,g_ocr_text/piano.jpg'
-                  className='card-img-top'
-                  alt='...'
-                  width={500}
-                  height={500}
-                />
-                <div className='card-body'>
-                  <p className='card-text'>Piano</p>
+            {image_list.map((item) => (
+              <div key={item.id} className='col'>
+                {console.log(item.url)}
+                <div className='card'>
+                  <Image
+                    src={item.url}
+                    className='card-img-top'
+                    alt='...'
+                    width={500}
+                    height={500}
+                  />
+                  <div className='card-body'>
+                    <p className='card-text'>{item.name}</p>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='col'>
-              <div className='card'>
-                <Image
-                  src='https://res.cloudinary.com/demo/image/fetch/c_fill,e_pixelate_region:30,h_80,w_200,x_170,y_260/http://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Austin_A110_Westminster_MkII_tail.jpg/550px-Austin_A110_Westminster_MkII_tail.jpg'
-                  className='card-img-top'
-                  alt='...'
-                  width={500}
-                  height={500}
-                />
-                <div className='card-body'>
-                  <p className='card-text'>Car Registration Number</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </main>
